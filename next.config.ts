7993a1ae3faf360c9702fd2e182370254ebf187c
Haruxe <haruxe@proton.me>
2024-11-05 19:24:44 -0800

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    optimizeCss: true,
+    optimisticClientCache: false,
+  },
 };
 
 export default nextConfig;
